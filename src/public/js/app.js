da73d967ac8033410df62587678028a8518560ce
LAPTOP-EI7PO15S\cloud099 <kdjkuku@gmail.com>
2023-03-25 17:26:21 +0900

    추가: node.js ws 설치, 닉네임-채팅

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,47 @@
-alert("hello");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nickname");
+
+// 서버로 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("message", () => {
+    console.log("Connected to Server")
+});
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server")
+});
+
+socket.addEventListener("message", (msg) => {
+    const li = document.createElement("li");
+
+    li.innerText = msg.data;
+    messageList.append(li);
+});
+
+function makeMsg(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+
+    const li = document.createElement("li");
+    li.innerText = `나: ${input.value}`;
+    messageList.append(li);
+
+    socket.send(makeMsg("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit() {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+
+    socket.send(makeMsg("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,49 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+// Http Server set
+const server = http.createServer(app);
+// WebSocket Server set
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+// 브라우저 연결
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from th Browser"));
+
+    sockets.push(socket);
+    socket["nickname"] = "익명";
+
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach((s) => {
+                    s.send(`${socket.nickname}: ${message.payload}`);
+                });
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
+
+    });
+});
+
+server.listen(3000, handleListen);
+
+
